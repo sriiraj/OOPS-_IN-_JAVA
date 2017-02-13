6af23a0bb755065d5ea775fd32a9854c842752e3
sriiraj.jaami <sriiraj.jaami12@gmail.com>
2017-02-13 23:38:37 +0530

    tutorial 1

---
+++ Ex2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex2;l
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author terror
+ */
+public class Ex2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner s=new Scanner(System.in);
+        String text=s.next();
+        System.out.println(text);
+        
+    } 
+    
+}
